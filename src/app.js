6fe60e90afdd99769804d816861a8c0941dd72db
kch1-jpeg <116844422+kch1-jpeg@users.noreply.github.com>
2022-10-31 21:14:41 +0000

    Correct d-flex

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
